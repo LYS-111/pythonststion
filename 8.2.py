75fd310abc92336da2223476020c80ba5496dd77
LYS-111 <lys891026@gmail.com>
2024-08-02 14:56:37 +0800

    Add files via upload

--- 8.2.py
+++ 8.2.py
@@ -6,21 +6,27 @@ Created on Fri Aug  2 12:58:31 2024
 """
 number=[]
 
-n=(input("請輸入次數:"))
+n=input("請輸入次數:")
 
 for i in range(100):
     if i==0:
-        number.append(1)    
+        number.append(1) 
+        print(list(number))
     elif i==1:
         a=1
         number=[1,a]
+        
     else:
-        100>i>=2,
+        100>i>=2
         temp1=number.pop(0)
         temp2=number.pop(1)
-        temp1+temp2==a
-        number.append(temp1,temp2,a)
-    print(number)   
+        b=int(temp1)+int(temp2)
+        if i==b:
+            number.append(temp1,temp2,a)
+        else:
+            i!=b
+            continue
+    print(list(number))   
 
 
 
