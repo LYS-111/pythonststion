9cb694987b2ed0d618e23debdaf2ff4df1308e68
LYS-111 <lys891026@gmail.com>
2024-08-02 14:17:13 +0800

    Add files via upload

---
+++ 8.2.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug  2 12:58:31 2024
+
+@author: lys
+"""
+number=[]
+
+n=(input("請輸入次數:"))
+
+for i in range(100):
+    if i==0:
+        number.append(1)    
+    elif i==1:
+        a=1
+        number=[1,a]
+    else:
+        100>i>=2,
+        temp1=number.pop(0)
+        temp2=number.pop(1)
+        temp1+temp2==a
+        number.append(temp1,temp2,a)
+    print(number)   
+
+
+
